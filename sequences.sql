cc7d33a1aa6ec329d1d4b79aef683dc501b49d2c
Joaquim Kennedy <joaquimkbs@gmail.com>
2023-08-26 12:53:50 -0300

    feat: ddl for sequences

---
+++ sequences.sql
@@ -0,0 +1,71 @@
+CREATE SEQUENCE "APP_LAB"."SEQ_BNF" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_BNF sequence usada para criar o id do BENEFICIARIO
+--------------------------------------------------------
+
+CREATE SEQUENCE "APP_LAB"."SEQ_BSL" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_BSL sequence usada para criar o id da BOLSA
+--------------------------------------------------------
+
+CREATE SEQUENCE "APP_LAB"."SEQ_CRS" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_CRS sequence usada para criar o id do CURSO
+--------------------------------------------------------
+
+CREATE SEQUENCE "APP_LAB"."SEQ_ITE" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_ITE sequence usada para criar o id da INSITUIÇAO DE ENSINO
+--------------------------------------------------------
+
+CREATE SEQUENCE "APP_LAB"."SEQ_MDL" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_MDL sequence usada para criar o id da MODALIDADE DO CURSO
+--------------------------------------------------------
+
+CREATE SEQUENCE "APP_LAB"."SEQ_TRN" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_TRN sequence usada para criar o id do TURNO DO CURSO
+--------------------------------------------------------
+
+CREATE SEQUENCE "APP_LAB"."SEQ_TPB" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_TPB sequence usada para criar o id do TIPO DE BOLSA
+--------------------------------------------------------
+
+CREATE SEQUENCE "APP_LAB"."SEQ_SEX" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_SEX sequence usada para criar o id do SEXO DO BENEFICIARIO
+--------------------------------------------------------
+
+CREATE SEQUENCE "APP_LAB"."SEQ_RCS" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_RCS sequence usada para criar o id da RACA D0 BENEFICIARIO
+--------------------------------------------------------
+
+CREATE SEQUENCE "APP_LAB"."SEQ_MUN" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_MUN sequence usada para criar o id do MUNICIPIO
+--------------------------------------------------------
+
+CREATE SEQUENCE "APP_LAB"."SEQ_UFS" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_UFS sequence usada para criar o id da UF
+--------------------------------------------------------
+
+CREATE SEQUENCE "APP_LAB"."SEQ_RGS" MINVALUE 1 MAXVALUE  9999999999999999999999999999
+INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE NOSCALE GLOBAL;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_RGS sequence usada para criar o id da REGIAO
+--------------------------------------------------------
